6afc13a507a3cef87cfaec6cf819a5b0da4f4f06
harshasudheer18 <harsha.sudheer@thoughtworks.com>
2023-04-25 17:59:34 +0530

    Add. pay button

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [isPayClicked, setIsPayClicked] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {!isPayClicked ?
+        <View style={styles.container}>
+          <Text>Click on Pay to continue</Text>
+          <TouchableOpacity style={styles.button} onPress={() => setIsPayClicked(true)}>
+            <Text style={styles.buttonText}>Pay</Text>
+          </TouchableOpacity>
+        </View>
+        :
+        <></>
+      }
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%'
   },
+  button: {
+    backgroundColor: "#2B85F3",
+    width: 80,
+    height: 40,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 40,
+    marginTop: 40
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18
+  }
 });
+
+export default App;
